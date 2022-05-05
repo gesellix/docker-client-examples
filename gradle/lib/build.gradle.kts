28027597afafc6fd8bf482bf835f476d21fd8f39
Tobias Gesellchen <tobias@gesellix.de>
2022-05-05 21:33:41 +0200

    update dependencies

--- gradle/lib/build.gradle.kts
+++ gradle/lib/build.gradle.kts
@@ -3,26 +3,12 @@ plugins {
 }
 
 repositories {
-  fun findProperty(s: String) = project.findProperty(s) as String?
-  listOf(
-    "docker-client/*",
-    "gesellix/*"
-  ).forEach { repo ->
-    maven {
-      name = "github"
-      setUrl("https://maven.pkg.github.com/$repo")
-      credentials {
-        username = System.getenv("PACKAGE_REGISTRY_USER") ?: findProperty("github.package-registry.username")
-        password = System.getenv("PACKAGE_REGISTRY_TOKEN") ?: findProperty("github.package-registry.password")
-      }
-    }
-  }
   mavenCentral()
 }
 
 dependencies {
-  implementation("de.gesellix:docker-client:2022-05-05T07-34-35")
-  runtimeOnly("org.slf4j:slf4j-simple:1.7.31")
+  implementation("de.gesellix:docker-client:2022-05-05T20-38-00")
+  runtimeOnly("org.slf4j:slf4j-simple:1.7.36")
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
