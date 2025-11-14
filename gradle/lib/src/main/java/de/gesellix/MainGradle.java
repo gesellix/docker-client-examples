b9196e37a1a8c5157191672cf60a32b7a6ba2eb9
Tobias Gesellchen <tobias@gesellix.de>
2025-11-14 21:40:34 +0100

    Chore

--- gradle/lib/src/main/java/de/gesellix/MainGradle.java
+++ gradle/lib/src/main/java/de/gesellix/MainGradle.java
@@ -3,14 +3,17 @@ package de.gesellix;
 import de.gesellix.docker.client.DockerClient;
 import de.gesellix.docker.client.DockerClientImpl;
 import de.gesellix.docker.client.EngineResponseContent;
+import de.gesellix.docker.remote.api.SystemInfo;
 import de.gesellix.docker.remote.api.SystemVersion;
 
 public class MainGradle {
 
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-    DockerClient client = new DockerClientImpl();
-    EngineResponseContent<SystemVersion> version = client.version();
-    System.out.println("version: " + version.getContent().getVersion());
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        DockerClient client = new DockerClientImpl();
+        EngineResponseContent<SystemVersion> version = client.version();
+        System.out.println("version: " + version.getContent().getVersion());
+        EngineResponseContent<SystemInfo> info = client.info();
+        System.out.println("info: " + info.getContent().toString());
+    }
 }
